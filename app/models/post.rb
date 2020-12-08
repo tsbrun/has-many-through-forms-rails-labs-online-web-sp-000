f2f0c4a621a6b74f2a0567ec08f1c546ad6713a4
TSephanya Brun <tsbrun@icloud.com>
2020-12-07 22:22:25 -0500

    commit tree b13e5acd4273e7f542b27922ac4cf2d551c845f0

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  # def category_ids=(category_ids)
+  #   category_ids.each do |id|
+  #     category = Category.find(id.to_i) unless id.empty?
+  #     self.categories << category
+  #   end
+  # end
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 end
